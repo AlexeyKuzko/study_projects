9b6ca6fabe70130ee92c067b2009e7002d65c37e
AlexeyKuzko <kuzkoalexey@icloud.com>
2023-09-02 17:00:38 +0300

    Solving problems in Yandex Academy Contest - Conditional Statements

--- ya_academy_contest_problems/python_basics/conditional_statements/problem_t.py
+++ ya_academy_contest_problems/python_basics/conditional_statements/problem_t.py
@@ -1,10 +1,21 @@
-string1 = input("")
-string2 = input("")
-string3 = input("")
-
-if "зайка" in string1:
-    print(string1, len(string1))
-elif "зайка" in string2:
-    print(string2, len(string2))
-else:
-    print(string3, len(string3))
+string_1 = str(input())
+string_2 = str(input())
+string_3 = str(input())
+if "зайка" in string_1 and "зайка" not in string_2 and "зайка" not in string_3:
+    print(string_1, len(string_1))
+elif "зайка" in string_2 and "зайка" not in string_1 and "зайка" not in string_3:
+    print(string_2, len(string_2))
+elif "зайка" in string_3 and "зайка" not in string_1 and "зайка" not in string_2:
+    print(string_3, len(string_3))
+elif "зайка" in string_1 and "зайка" in string_2 and "зайка" not in string_3:
+    output = min(string_1, string_2)
+    print(f"{output} {len(output)}")
+elif "зайка" in string_1 and "зайка" in string_3 and "зайка" not in string_2:
+    output = min(string_1, string_3)
+    print(f"{output} {len(output)}")
+elif "зайка" in string_2 and "зайка" in string_3 and "зайка" not in string_1:
+    output = min(string_2, string_3)
+    print(f"{output} {len(output)}")
+elif "зайка" in string_1 and "зайка" in string_2 and "зайка" in string_3:
+    output = min(string_1, string_2, string_3)
+    print(f"{output} {len(output)}")
