ef22895be1e390f98b43920f90d13665d21cdd01
AlexeyKuzko <kuzkoalexey@icloud.com>
2023-09-02 15:35:44 +0300

    Solving problems in Yandex Academy Contest - Input Output and Formatting

---
+++ ya_academy_contest_problems/python_basics/input_output_formatting/problem_r.py
@@ -0,0 +1,6 @@
+binary = input("")
+decimal = int(input(""))
+binary_decimal = int(binary, 2)
+result = binary_decimal - decimal
+
+print(result * (-1))
---
+++ ya_academy_contest_problems/python_basics/input_output_formatting/problem_t.py
@@ -0,0 +1,10 @@
+# n_weight = int(input(""))
+# m_price = int(input(""))
+# k1_price = int(input(""))
+# k2_price = int(input(""))
+# i = 1
+# while n_weight > i:
+#    if n_weight * k1_price + i * k2_price == n_weight * m_price:
+#        n_weight -= 1
+#         i += 1
+# print(n_weight, i)
