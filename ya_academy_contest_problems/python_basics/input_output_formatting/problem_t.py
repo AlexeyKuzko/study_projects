1d61c6b12888dae84abc719fc6488e21ece95747
AlexeyKuzko <kuzkoalexey@icloud.com>
2023-09-03 14:07:07 +0300

    solved all problems here

--- ya_academy_contest_problems/python_basics/input_output_formatting/problem_t.py
+++ ya_academy_contest_problems/python_basics/input_output_formatting/problem_t.py
@@ -1,10 +1,7 @@
-# n_weight = int(input(""))
-# m_price = int(input(""))
-# k1_price = int(input(""))
-# k2_price = int(input(""))
-# i = 1
-# while n_weight > i:
-#    if n_weight * k1_price + i * k2_price == n_weight * m_price:
-#        n_weight -= 1
-#         i += 1
-# print(n_weight, i)
+N = int(input(""))
+M = int(input(""))
+K1 = int(input(""))
+K2 = int(input(""))
+m1 = N * (K2 - M) / (K2 - K1)
+m2 = N - m1
+print(int(m1), int(m2))
