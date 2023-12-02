e0bfa56a85ec526c82da1fac3385f36c4ccb4a65
AlexeyKuzko <kuzkoalexey@icloud.com>
2023-12-02 18:12:56 +0300

    5th task done

--- ya_practicum_aqa_projects/qa_python_1/task_5.py
+++ ya_practicum_aqa_projects/qa_python_1/task_5.py
@@ -13,7 +13,7 @@ class TestCase:
         self.result = result
 
     def get_test_case(self):
-        return {'Шаги': self.steps, 'Ожидаемый результат': self.result}
+        print({'Шаги': self.steps, 'Ожидаемый результат': self.result})
 
 
 test_case_1 = TestCase()
@@ -23,11 +23,11 @@ test_case_1.delete_step(3)
 test_case_1.set_step(2, 'Перейти в раздел Товары')
 test_case_1.set_step(3, 'Нажать кнопку «В корзину» у первого товара')
 test_case_1.set_result('Товар окажется в корзине')
-print(test_case_1.get_test_case())
+test_case_1.get_test_case()
 
 test_case_2 = TestCase()
 test_case_2.set_step(1, 'Перейти на сайт')
 test_case_2.set_step(2, 'Перейти в раздел Корзина')
 test_case_2.set_step(3, 'Нажать кнопку "Удалить"')
 test_case_2.set_result('Товар удален из корзины')
-print(test_case_2.get_test_case())
+test_case_2.get_test_case()
