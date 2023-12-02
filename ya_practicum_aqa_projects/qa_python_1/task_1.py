9739a357130daaa7d25477e4002089e6998797b4
AlexeyKuzko <kuzkoalexey@icloud.com>
2023-12-02 18:15:17 +0300

    all tasks done

--- ya_practicum_aqa_projects/qa_python_1/task_1.py
+++ ya_practicum_aqa_projects/qa_python_1/task_1.py
@@ -1,19 +1,19 @@
-STRING = "1h 45m,360s,25m,30m 120s,2h 60s"
-values_of_string = STRING.replace(" ", "").split(",")
+STRING = '1h 45m,360s,25m,30m 120s,2h 60s'
+values_of_string = STRING.replace(' ', '').split(',')
 minutes_counter = 0
 
 for value in values_of_string:
-    if "h" in value:
-        minutes_counter += int(value.split("h")[0]) * 60
-        if "m" in value:
-            minutes_counter += int((value.split("h"))[1].replace("m", ""))
-        elif "s" in value:
-            minutes_counter += int((value.split("h"))[1].replace("s", "")) // 60
-    elif "m" in value:
-        minutes_counter += int(value.split("m")[0])
-        if "s" in value:
-            minutes_counter += int((value.split("m"))[1].replace("s", "")) // 60
-    elif "s" in value:
-        minutes_counter += int(value.split("s")[0]) // 60
+    if 'h' in value:
+        minutes_counter += int(value.split('h')[0]) * 60
+        if 'm' in value:
+            minutes_counter += int((value.split('h'))[1].replace('m', ''))
+        elif 's' in value:
+            minutes_counter += int((value.split('h'))[1].replace('s', '')) // 60
+    elif 'm' in value:
+        minutes_counter += int(value.split('m')[0])
+        if 's' in value:
+            minutes_counter += int((value.split('m'))[1].replace('s', '')) // 60
+    elif 's' in value:
+        minutes_counter += int(value.split('s')[0]) // 60
 
 print(minutes_counter)
