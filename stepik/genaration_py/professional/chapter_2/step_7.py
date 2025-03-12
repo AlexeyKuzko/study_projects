a2e7d6366436fac04c16208a29a8941fc411d27b
AlexeyKuzko <kuzkoalexey@icloud.com>
2025-03-12 13:07:10 +0300

    new solutionы

---
+++ stepik/genaration_py/professional/chapter_2/step_7.py
@@ -0,0 +1,2 @@
+def is_valid(string):
+    return len(string) in {4, 5, 6} and all(c in '0123456789' for c in string)
---
+++ stepik/genaration_py/professional/chapter_2/step_8.py
@@ -0,0 +1,6 @@
+def print_given(*args, **kwargs):
+    for arg in args:
+        print(arg, type(arg))
+
+    for key in sorted(kwargs):
+        print(key, kwargs[key], type(kwargs[key]))
