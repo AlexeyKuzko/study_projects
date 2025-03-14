b2b7a488da18a985a32bc4818177ec481a276864
AlexeyKuzko <kuzkoalexey@icloud.com>
2025-03-14 10:04:47 +0300

    Refactor string handling and add new function for likes

--- stepik/genaration_py/professional/chapter_2/step_5.py
+++ stepik/genaration_py/professional/chapter_2/step_5.py
@@ -1,5 +1,5 @@
 def hide_card(card_number):
     # Убираем все пробелы из строки
-    cleaned_card_number = ''.join(card_number.split())
+    cleaned_card_number = "".join(card_number.split())
     # Заменяем первые 12 цифр на *
-    return '*' * 12 + cleaned_card_number[-4:]
+    return "*" * 12 + cleaned_card_number[-4:]
