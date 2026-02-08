c8bc46e40a94081e554627f7a6805545c7f164e5
AlexeyKuzko <kuzkoalexey@icloud.com>
2026-02-08 20:10:30 +0300

    solved day_3

--- specialist/day_3/Homework/full_game.py
+++ specialist/day_3/Homework/full_game.py
@@ -1,22 +1,190 @@
 """Создадим консольную игру “Дурака без козырей”"""
-from day_2.card import Card
-from day_2.deck import Deck
+import random
 
-"""
-1. Создайте колоду из 52 карт. Перемешайте ее.
-2. Первый игрок берет сверху 10 карт
-3. Второй игрок берет сверху 10 карт.
-4. Игрок-1 ходит:
-    4.1. игрок-1 выкладывает самую маленькую карту по "старшенству"
-    4.2. игрок-2 пытается бить карту, если у него есть такая же масть, но значением больше.
-    4.3. Если игрок-2 не может побить карту, то он забирает ее себе в руку
-    4.4. Если игрок-2 бьет карту, то игрок-1 может подкинуть карту любого значения, которое есть на столе.
-5. Если Игрок-2 отбился, то Игрок-1 и Игрок-2 меняются местами. Игрок-2 ходит, Игрок-1 отбивается.
-6. Выведите в консоль максимально наглядную визуализацию данных ходов (можно дополнительно использовать библиотеку rich)
-7* Реализовать возможность добрать карты из колоды после того, как один из игроков отбился/взял в руку
+VALUES = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+SUITS = ('Spades', 'Clubs', 'Diamonds', 'Hearts')
+SUITS_UNI = {
+        'Spades': '♠',
+        'Clubs': '♣',
+        'Diamonds': '♦',
+        'Hearts': '♥'
+}
 
-Игра ведется до тех пор, пока у кого-нибудь из игроков не останется ни одной карты
-(если реализован 7 пункт, то колода должна быть пуста).
-Игрок, у которого нет карт, объявляется победителем.
-Если при последнем отбое заканчиваются карты и у первого игрока и у второго, то результат игры - ничья.
-"""
+class Card:
+    """Карты в колоде должны являться объектами - экземплярами класса Card"""
+
+    def __init__(self, value: str, suit: str):
+        """При создании конструктор карты принимает: значение карты и ее масть."""
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def __str__(self):
+        return self.to_str()
+
+    def __lt__(self, other):
+        return (VALUES.index(self.value), SUITS.index(self.suit)) < (VALUES.index(other.value),SUITS.index(other.suit))
+
+    def to_str(self) -> str:
+        """Метод возвращает строковое представление карты в виде строки, формата:4♦"""
+        return f"{self.value}{SUITS_UNI[self.suit]}"
+
+    def equal_suit(self, other_card) -> bool:
+        """Метод проверяет, одинаковые ли масти у карт"""
+        return self.suit == other_card.suit
+
+    def more(self, other_card) -> bool:
+        """Возвращает True, если карта у которой вызван метод больше,
+        чем карта, которую передали в качестве параметра"""
+        return ((VALUES.index(self.value), SUITS.index(self.suit)) >
+                (VALUES.index(other_card.value), SUITS.index(other_card.suit)))
+
+    def less(self, other_card) -> bool:
+        """Метод проверяет, является ли карта младше, чем карта в параметре"""
+        return not self.more(other_card)
+
+
+class Deck:
+    """Колода состоит из 52 карт (объектов класса Card)"""
+
+    def __init__(self):
+        """При создании новой колоды все карты должны находиться в отсортированном порядке -
+        сначала идут все пики от 2-ки до туза, затем крести, буби и червы."""
+        self.cards = []
+        for suits in SUITS:
+            for value in VALUES:
+                self.cards.append(Card(value, suits))
+
+    def __len__(self):
+        return len(self.cards)
+
+    def __str__(self):
+        return ", ".join(card.to_str() for card in self.cards)
+
+    def shuffle(self):
+        """Метод перемешивает колоду, располагая карты в случайном порядке"""
+        random.shuffle(self.cards)
+
+    def draw(self, x: int):
+        """Возвращает x первых карт из колоды в виде списка, эти карты убираются из колоды.
+        Уточнение: первую карту в списке считаем верхней картой колоды"""
+        draw_x = self.cards[:x]
+        self.cards = self.cards[x:]
+        return draw_x
+
+    def show(self):
+        """Отображает все карты колоды в формате:
+        deck[12]: 3♥, 4♦, A♣, …
+        где 12 - текущее кол-во карт в колоде"""
+        print(f"deck[{len(self.cards)}]: {self}")
+
+
+class Player:
+    def __init__(self, name: str, cards: list[Card]):
+        self.name = name
+        self.hand = cards
+
+    def sort_hand(self):
+        """Сортировка карт по старшинству"""
+        self.hand.sort()
+
+    def show_hand(self):
+        return ", ".join(str(card) for card in self.hand)
+
+    def has_cards(self) -> bool:
+        return len(self.hand) > 0
+
+    def take_cards(self, cards: list[Card]):
+        self.hand.extend(cards)
+        self.sort_hand()
+
+    def remove_card(self, card: Card):
+        self.hand.remove(card)
+
+    def get_lowest_card(self) -> Card:
+        """Самая маленькая карта"""
+        self.sort_hand()
+        return self.hand[0]
+
+    def try_beat(self, attack_card: Card) -> Card | None:
+        possible = [
+            card for card in self.hand
+            if card.equal_suit(attack_card) and card.more(attack_card)
+        ]
+
+        if not possible:
+            return None
+
+        # найдём минимальную вручную через less
+        min_card = possible[0]
+        for card in possible[1:]:
+            if card.less(min_card):
+                min_card = card
+
+        return min_card
+
+    def refill_from_deck(self, deck: Deck, limit: int = 10):
+        """Добор карт до 10"""
+        while len(self.hand) < limit and deck.cards:
+            self.hand.append(deck.draw(1)[0])
+        self.sort_hand()
+
+
+deck = Deck()
+deck.shuffle()
+
+player1 = Player("Игрок-1", deck.draw(10))
+player2 = Player("Игрок-2", deck.draw(10))
+
+attacker = player1
+defender = player2
+
+table = []
+
+
+# --- Игровой цикл ---
+
+while player1.has_cards() and player2.has_cards():
+
+    print(f"\nХодит {attacker.name}")
+    print(f"{attacker.name}: {attacker.show_hand()}")
+    print(f"{defender.name}: {defender.show_hand()}")
+
+    # 1. Атакующий кладёт самую маленькую карту
+    attack_card = attacker.get_lowest_card()
+    attacker.remove_card(attack_card)
+    table = [attack_card]
+
+    print(f"{attacker.name} ходит: {attack_card}")
+
+    # 2. Защитник пытается побить
+    beat_card = defender.try_beat(attack_card)
+
+    if beat_card is None:
+        # 3. Не смог побить → забирает карты
+        print(f"{defender.name} не смог побить и забирает карты")
+        defender.take_cards(table)
+    else:
+        defender.remove_card(beat_card)
+        table.append(beat_card)
+        print(f"{defender.name} бьёт картой: {beat_card}")
+
+        # 5. Если отбился — меняемся ролями
+        attacker, defender = defender, attacker
+
+    # 7. Добор карт
+    attacker.refill_from_deck(deck)
+    defender.refill_from_deck(deck)
+
+    table.clear()
+
+
+# --- Завершение игры ---
+
+print("\nИгра окончена!")
+
+if not player1.has_cards() and not player2.has_cards():
+    print("Ничья!")
+elif not player1.has_cards():
+    print("Победил Игрок-1!")
+else:
+    print("Победил Игрок-2!")
--- specialist/day_3/Homework/rect_magic_methods.py
+++ specialist/day_3/Homework/rect_magic_methods.py
@@ -1,32 +1,60 @@
-"""
-Используем класс Rect() с длиной и шириной в качестве атрибутов
-задания на magic methods:
-1) __repr__() 
-2) __str__()
+class Rect:
+    def __init__(self, length: float, width: float):
+        self.length = length
+        self.width = width
 
-3) r1 * 5 (__mul__()) - обе стороны станут в 5 раз больше
-   добавить проверкy, что тип аргумента метода __mul__ это int или float
-   # Variant 1, но python -O отключает все assert'ы
-   assert type(arg) in (int, float), 'Bad type'
+    # площадь — пригодится для сравнений
+    def area(self):
+        return self.length * self.width
 
-   # Variant 2
-   if type(arg) in (int, float):
-       pass
-   else:
-       raise TypeError
+    # 1) repr — для разработчика
+    def __repr__(self):
+        return f"Rect(length={self.length}, width={self.width})"
 
-4) r1 < r2, r1 == r2, r1 <= r1 и т.п.
+    # 2) str — для пользователя
+    def __str__(self):
+        return f"Прямоугольник {self.length} x {self.width}"
 
-Шесть методов для сравнения:
-__lt__() -> '<'
-__gt__() -> '>'
-__le__() -> '<='
-__ge__() -> '>='
-__eq__() -> '=='
-__ne__() -> '!='
-Сравнить по площади.
+    # 3) умножение на число
+    def __mul__(self, arg):
+        if type(arg) in (int, float):
+            return Rect(self.length * arg, self.width * arg)
+        else:
+            raise TypeError("Multiplier must be int or float")
 
-def __gt__(self, other):
-	# ...
-	# return True/False
-"""
+    # чтобы работало 5 * r1
+    def __rmul__(self, arg):
+        return self.__mul__(arg)
+
+    # 4) сравнение по площади
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+
+r1 = Rect(2, 3)
+r2 = Rect(4, 5)
+
+print(r1)              # str
+print(repr(r1))        # repr
+
+r3 = r1 * 5
+print(r3)
+
+print(r1 < r2)
+print(r1 == r2)
+print(r1 <= r1)
