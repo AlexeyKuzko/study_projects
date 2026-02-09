a0da5466a7533cd95ebb12d5448abfb9afe793a2
AlexeyKuzko <kuzkoalexey@icloud.com>
2026-02-09 19:03:22 +0300

    solved rect

--- specialist/day_3/Homework/rect_magic_methods.py
+++ specialist/day_3/Homework/rect_magic_methods.py
@@ -1,60 +1,69 @@
+"""Класс Rect с демонстрацией работы магических методов"""
+
 class Rect:
-    def __init__(self, length: float, width: float):
+    """Класс прямоугольника"""
+    def __init__(self, length=1.2, width=3):  # пре
+        """Конструктор с присвоением атрибутов класса - длины и ширины"""
         self.length = length
         self.width = width
-
-    # площадь — пригодится для сравнений
-    def area(self):
-        return self.length * self.width
-
-    # 1) repr — для разработчика
+    # задания на magic methods: 1) __repr__() 2) __str__()
     def __repr__(self):
-        return f"Rect(length={self.length}, width={self.width})"
+        """Метод возвращает точное строковое представление объекта (для разработчика)"""
+        return f"Rect({self.length=}, {self.width=})"
 
-    # 2) str — для пользователя
     def __str__(self):
-        return f"Прямоугольник {self.length} x {self.width}"
+        """Метод возвращает удобное строковое представление объекта (для пользователя)"""
+        return f"Прямоугольник: длина {self.length} и ширина {self.width}"
 
-    # 3) умножение на число
+    # 3) умножение на число методом __mul__
     def __mul__(self, arg):
+        """Метод увеличивает прямоугольник в arg раз"""
         if type(arg) in (int, float):
             return Rect(self.length * arg, self.width * arg)
         else:
-            raise TypeError("Multiplier must be int or float")
+            raise TypeError("Тип аргумента метода __mul__ должен быть int или float")
 
-    # чтобы работало 5 * r1
-    def __rmul__(self, arg):
-        return self.__mul__(arg)
+    # 4) сравнения прямоугольников по площади
+    def area(self):
+        """Метод возвращает площадь прямоугольника, можно использовать для дальнейших сравнений"""
+        return self.length * self.width
 
-    # 4) сравнение по площади
     def __lt__(self, other):
+        """Меньше ли площадь прямоугольника, чем у other прямоугольника"""
         return self.area() < other.area()
 
     def __gt__(self, other):
+        """Больше ли площадь прямоугольника, чем у other прямоугольника"""
         return self.area() > other.area()
 
     def __le__(self, other):
+        """'Меньше или равна' площадь прямоугольника, чем у other прямоугольника"""
         return self.area() <= other.area()
 
     def __ge__(self, other):
+        """'Больше или равна' площадь прямоугольника, чем у other прямоугольника"""
         return self.area() >= other.area()
 
     def __eq__(self, other):
+        """Равна ли площадь прямоугольника площади other прямоугольника"""
         return self.area() == other.area()
 
     def __ne__(self, other):
+        """Не равна ли площадь прямоугольника площади other прямоугольника"""
         return self.area() != other.area()
 
 
-r1 = Rect(2, 3)
-r2 = Rect(4, 5)
+r1 = Rect()
+r2 = Rect(4, 5.67)
+r3, r4 = r1 * 5, r2 * 0.42   # mul
 
-print(r1)              # str
-print(repr(r1))        # repr
+print(f"Даны прямоугольники (экземпляры): "
+      f"\n{r1} ({repr(r1)})\n{r2} ({repr(r2)})\n{r3} ({repr(r3)})\n{r4} ({repr(r4)})")  # str и repr
 
-r3 = r1 * 5
-print(r3)
-
-print(r1 < r2)
-print(r1 == r2)
-print(r1 <= r1)
+# сравнения, которым тоже можно написать f-стринги с объяснением, что с чем сравниваем
+print(r1 < r2)  # lt
+print(r3 > r4)  # gt
+print((r1 <= r4) is True) # le (решил немного поиграться)
+print(not r2 >= r3) # ge (пусть везде будет True для красоты)
+print((r4 == r2) is False)  # eq
+print((r3 != r3) != True)  # ne
